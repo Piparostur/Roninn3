8d7fc62ccc60a8c9a9fa65245d056929dbcce8e6
Addilius <addilius@gmail.com>
2023-02-07 21:40:07 +0000

    hvað í andskotanum er ég að gera

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -2,6 +2,7 @@ import Phaser from 'phaser'
 
 export default class HelloWorldScene extends Phaser.Scene
 {
+    load: any;
 	constructor()
 	{
 		super('hello-world')
@@ -9,11 +10,17 @@ export default class HelloWorldScene extends Phaser.Scene
 
 	preload()
     {
+        this.load.image('sky', 'assets/sky.png');
+        this.load.image('ground', 'assets/platform.png');
+        this.load.image('star', 'assets/star.png');
+        this.load.image('bomb', 'assets/bomb.png');
+        this.load.spritesheet('dude', 'assets/dude.png', { frameWitdh: 30, FrameHeight: 48});
+
     }
 
     create()
     {
-
+        this.add.image(400, 300, 'sky')
     }
     update()
     {
