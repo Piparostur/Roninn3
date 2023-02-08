27d6530d59e9e7f75f29a7f0e0b7359a1ea9ed0d
Piparostur <birgirrs21@ru.is>
2023-02-08 01:20:11 +0000

    Bætti sprite við, lagaði physics þannig gæjinn lendur ofan á grasinu. Bætti síðan við movement (L/R) og jump (SPACE) og stillti þannig hann getur bara hoppað ef hann snertir jörðina (no jetpack).

--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -1,41 +1,58 @@
-import Phaser from 'phaser'
+import Phaser from 'phaser';
 
-export default class HelloWorldScene extends Phaser.Scene
-{
-    private platforms?: Phaser.Physics.Arcade.StaticGroup
-	constructor()
-	{
-		super('hello-world')
-	}
+export default class HelloWorldScene extends Phaser.Scene {
+  private platforms?: Phaser.Physics.Arcade.StaticGroup;
+  private player?: Phaser.Physics.Arcade.Sprite;
+  private cursors?: Phaser.Types.Input.Keyboard.CursorKeys;
 
-    preload()
+  constructor() {
+    super('hello-world');
+  }
 
-    {
-        this.load.image('sky', 'assets/sky.png');
-        this.load.image('ground', 'assets/platform.png');
-        this.load.image('star', 'assets/star.png');
-        this.load.image('bomb', 'assets/bomb.png');
-        this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48});
+  preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
+  }
+
+  create() {
+    this.add.image(400, 300, 'sky');
+
+    // Add platforms
+    this.platforms = this.physics.add.staticGroup({
+        key: 'ground',
+        repeat: 11,
+        setXY: { x: 150, y: 550, stepX: 70 },
+      });
+
+    this.platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    this.platforms.create(600, 400, 'ground');
+    this.platforms.create(50, 250, 'ground');
+    this.platforms.create(750, 220, 'ground');
+
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+
+    // Set up cursors
+    this.cursors = this.input.keyboard.createCursorKeys();
+
+    this.physics.add.collider(this.player, this.platforms);
+  }
+
+  update() {
+    if (this.cursors!.left!.isDown) {
+      this.player!.setVelocityX(-160);
+    } else if (this.cursors!.right!.isDown) {
+      this.player!.setVelocityX(160);
+      //jump on space but only if touching the ground
+    } else if (this.cursors!.space!.isDown && this.player!.body.touching.down) {
+        this.player!.setVelocityY(-160);
+    } else {
+      this.player!.setVelocityX(0);
     }
-
-
-    create()
-    {
-        this.add.image(400, 300, 'sky');
-        
-        this.platforms = this.physics.add.staticGroup()
-        const ground = this.platforms.create(400, 568, 'ground') as Phaser.Physics.Arcade.Sprite
-        
-        ground
-            .setScale(2)
-            .refreshBody()
-            
-        this.platforms.create(600, 400, 'ground')
-        this.platforms.create(50, 250, 'ground')
-        this.platforms.create(750,220, 'ground')
-    }
-    update()
-    {
-        
-    }
+  }
 }
